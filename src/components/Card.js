3b4607f2e8311fd26e11d5a98ced710235b72648
KamilDike <40762724+KamilDike@users.noreply.github.com>
2021-02-03 16:36:10 +0100

    Initialization

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Item from './Item'
+
+function Card() {
+    return (
+        <div className='card'>
+            <Item/>
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/Item.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import tree from './../images/tree.png'; 
+
+function Item() {
+    return (
+        <div className="item">
+            <img src={tree} alt="tree"/>
+        </div>
+    )
+}
+
+export default Item
